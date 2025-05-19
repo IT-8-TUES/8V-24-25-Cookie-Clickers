345931c8eb18cb7b6aa8bf20e58c14d4a4531cea
Jeleff-cod3 <jeleffv@gmail.com>
2025-05-19 15:51:29 +0300

    class creation almost done

--- GradePro/classes/models.py
+++ GradePro/classes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from accounts.models import StudentProfile
 
 # Create your models here.
+class Class(models.Model):
+    subject = models.CharField(max_length=50)
+    students = models.ManyToManyField(StudentProfile, related_name='students')
+    
