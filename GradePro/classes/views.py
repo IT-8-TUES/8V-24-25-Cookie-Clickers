e18e6ee1048f90dd0ce39fcc68aff71ed93ac215
Kir4o-code <kiril.kirilov.eliseev@gmail.com>
2025-05-24 14:11:29 +0300

    C grade color change

--- GradePro/classes/views.py
+++ GradePro/classes/views.py
@@ -89,7 +89,10 @@ def profile_page(request):
                 mate_grades = Grades.objects.filter(student=mate, school_class=selected_class)
                 mate_values = []
                 for g in mate_grades:
-                    mate_values.extend(g.values)
+                    if isinstance(g.values, list):
+                        mate_values.extend(g.values)
+                    elif isinstance(g.values, int):
+                        mate_values.append(g.values)
                 avg = round(sum(mate_values) / len(mate_values), 2) if mate_values else 0
                 ranking.append((mate, avg))
 
@@ -115,7 +118,10 @@ def profile_page(request):
                 mate_values = []
                 mate_grades = Grades.objects.filter(student=mate)
                 for g in mate_grades:
-                    mate_values.extend(g.values)
+                    if isinstance(g.values, list):
+                        mate_values.extend(g.values)
+                    elif isinstance(g.values, int):
+                        mate_values.append(g.values)
                 avg = round(sum(mate_values) / len(mate_values), 2) if mate_values else 0
                 school_ranking.append((mate, avg))
 
